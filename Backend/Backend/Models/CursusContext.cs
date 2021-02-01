b4703e8a4420ec094768c8af1d4ba1f12f006c44
Bas Mattern <notgoingtohappen@live.nl>
2021-02-01 15:54:11 +0100

    backend opnieuw

---
+++ Backend/Backend/Models/CursusContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace Backend.Models
+{
+    public class CursussenContext : DbContext
+    {
+        public CursussenContext()
+        {
+            Database.SetInitializer<CursussenContext>(new DropCreateDatabaseIfModelChanges<CursussenContext>());
+        }
+        public DbSet<Cursus> Cursussen { get; set; }
+        public DbSet<CursusInstantie> CursusInstanties { get; set; }
+    }
+}
